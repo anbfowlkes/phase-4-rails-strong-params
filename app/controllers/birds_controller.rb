e2fd306b5b69ea07a0f501ca2366e147356a559c
Aaron Fowlkes <anbfowlkes@gmail.com>
2022-09-04 01:30:20 -0400

    commit tree c8bd0512ef6b0590ded529f68eeb58105b2a2f10

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,8 +8,9 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
+    # bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
+    render json: bird, status: 201
   end
 
   # GET /birds/:id
@@ -21,5 +22,12 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create]
+  # resources :birds, only: [:index, :show, :create]
+
+  get '/birds', to: 'birds#index'
+  get '/birds/:id', to: 'birds#show'
+  post '/birds', to: 'birds#create'
+
+
 end
